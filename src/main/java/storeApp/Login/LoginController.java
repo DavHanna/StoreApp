a677d3165fefd42968c41db8b9ed574c5eb2b6c4
DavHanna <contact@david-hanna.com>
2018-03-11 11:16:54 +0200

    Refreshed the pom.properties Removed the Repository class from the user repository

--- src/main/java/storeApp/Login/LoginController.java
+++ src/main/java/storeApp/Login/LoginController.java
@@ -2,6 +2,7 @@ package storeApp.Login;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,25 +12,29 @@ import storeApp.User.User;
 import storeApp.User.UserRepository;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
 	
 	@Autowired
 	UserRepository userRepo;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String index() 
+	@GetMapping("/login")
+	public User index() 
 	{
-		return "login";
+		User user = userRepo.findByUsername("user");
+//		
+		return user;
+//		return "login";
 	}
 	
-	@RequestMapping(method=RequestMethod.POST)
-	public String login(@RequestParam("username") String username, @RequestParam("password") String password)
-	{
-		User user = userRepo.findByUsername(username);
-		if (user == null || user.password != password)
-			return "wrongCredentials";
-		
-		return "addProduct";
-	}
+//	@RequestMapping(method=RequestMethod.POST)
+//	public User login(@RequestParam("username") String username, @RequestParam("password") String password)
+//	{
+//		User user = userRepo.findByUsername(username);
+//		
+//		return user;
+//		if (user == null || user.password != password)
+//			return "wrongCredentials";
+//		
+//		return "addProduct";
+//	}
 }
--- src/main/java/storeApp/User/UserRepository.java
+++ src/main/java/storeApp/User/UserRepository.java
@@ -3,7 +3,7 @@ package storeApp.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
 
-public interface UserRepository extends CrudRepository<User, Integer>, Repository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 
 	public User findByUsername(String username);
 }
