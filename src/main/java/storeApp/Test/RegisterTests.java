f8e729e3019d66a0e0e041887c62c33456b62892
maryam ahmed <mariamahmed.96@hotmail.com>
2018-05-03 12:37:04 +0200

    Modified RegisterTests.

--- src/main/java/storeApp/Test/RegisterTests.java
+++ src/main/java/storeApp/Test/RegisterTests.java
@@ -1,6 +1,8 @@
 package storeApp.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -10,14 +12,17 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
 
 import storeApp.Register.RegisterController;
+import storeApp.User.User;
 import storeApp.User.UserRepository;
 
 public class RegisterTests {
 
 	@Mock private HttpSession mockHttpSession;
 	@Mock private UserRepository userRepo;
+	@Mock private Model model;
 	
 	@InjectMocks
 	@Resource
@@ -30,8 +35,21 @@ public class RegisterTests {
 
 	@Test
 	public void testRegister() {
-		String result = this.ctrl.register("khalid", "secret", "Store Owner");
+		String result = this.ctrl.register("khalid", "secret", "Store Owner", model);
 		assertEquals("redirect:/login", result);
+	}
+	
+	@Test
+	public void testInvalidRegister() {
+		when(userRepo.findByUsername(anyString())).thenReturn(new User());
+		String result = this.ctrl.register("khalid", "secret", "Store Owner", model);
+		assertEquals("message", result);
+	}
+
+	@Test
+	public void testEmptyRegister() {
+		String result = this.ctrl.register("", "", "", model);
+		assertEquals("message", result);
 	}
 
 }
