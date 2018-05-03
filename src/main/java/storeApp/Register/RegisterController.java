cd5638d8f89a33f9360ae9bb4d0ef997eaf537fa
maryam ahmed <mariamahmed.96@hotmail.com>
2018-05-03 12:36:21 +0200

    Added validation in RegisterController.

--- src/main/java/storeApp/Register/RegisterController.java
+++ src/main/java/storeApp/Register/RegisterController.java
@@ -2,6 +2,7 @@ package storeApp.Register;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,9 +25,22 @@ public class RegisterController {
 	
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public String register(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("type") String type)
+	public String register(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("type") String type,Model model)
 	{
-		User user = new User(username, password, type);
+		User user= userRepo.findByUsername(username);
+		if(user!= null)
+		{
+			model.addAttribute("message",  "Username already taken");
+			return "message";
+		}
+		
+		if(username.equals("")|| password.equals("") || type.equals(""))
+		{
+			model.addAttribute("message",  "please fill the form");
+			return "message";
+		}
+	     user = new User(username, password, type);
+		
 		userRepo.save(user);
 		// Redirect user to the home page
 		return "redirect:/login";
